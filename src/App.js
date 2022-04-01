5e3e8d69ea60106e6dc5165ec0a9fa990ea07390
Disha <bidisha2125@gmail.com>
2022-04-01 19:06:40 +0530

    Created this for making amazon clone

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.css";
+import Header from "./Header";
+import Home from "./Home";
+import Checkout from "./Checkout";
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    //BEM
+    <Router>
+      <div className="app">
+        <Routes>
+          <Route path="/checkout" element={[<Header />, <Checkout />]} />
+          <Route path="/" element={[<Header />, <Home />]} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDE_uQAN7Tykvyl5K25LtG2x8DpjV9MCEs",
+    authDomain: "clone-b8c46.firebaseapp.com",
+    projectId: "clone-b8c46",
+    storageBucket: "clone-b8c46.appspot.com",
+    messagingSenderId: "186562434280",
+    appId: "1:186562434280:web:0892c404d17c6f1911255d",
+    measurementId: "G-8CWFGS7W1M"
+  };
